24c5092dbb1a1c5d505afd1a3eaab6e657ca2a02
Kamil Pietrusiński <s165435@student.pg.edu.pl>
2022-11-07 22:43:24 +0100

    Update main.py - add loop that updates info about server status in bot status and command that print server status in chat Update mc_api.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import asyncio
-from discord.ext import commands
+from discord.ext import commands, tasks
 from mc_api import get_server_status
 
 intents = discord.Intents.default()
@@ -12,18 +12,30 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print('Bot {0.user} '.format(bot) + ' started')
+    update_status.start()
+
+
+@bot.command(name='status', brief="Prints current server status", description="Prints current server status")
+# !status
+async def print_status(ctx):
+    is_online, number_of_players = get_server_status()
+    if is_online:
+        await ctx.send("Serwer jest online, liczba graczy: {}".format(number_of_players))
+        await bot.change_presence(status=discord.Status.online,
+                                  activity=discord.Game("Online, liczba graczy: {}".format(number_of_players)))
+    else:
+        await ctx.send("Serwer jest offline")
+        await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game("Offline"))
+
+
+@tasks.loop(minutes=5)
+async def update_status():
     is_online, number_of_players = get_server_status()
     if is_online:
         await bot.change_presence(status=discord.Status.online,
                                   activity=discord.Game("Online, liczba graczy: {}".format(number_of_players)))
     else:
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("Offline"))
+        await bot.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game("Offline"))
 
-
-@bot.command(name='players', brief="",
-             description="")
-# !players
-async def print_players_names(ctx, arg):
-    pass
 
 bot.run(os.getenv('TOKEN'))
--- mc_api.py
+++ mc_api.py
@@ -3,10 +3,11 @@ import requests
 
 
 def get_server_status():
-    response = requests.get((os.getenv('URL')))
+    response = requests.get(str(os.getenv('URL')))
     data = response.json()
 
     is_online = (data['online'])
     number_of_players = (data['players']['online'])
 
     return is_online, number_of_players
+
